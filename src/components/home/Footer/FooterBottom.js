584a81e99cd566ebd797f05b2d9e64a2b126c534
CodingBlow <mdfarhanraza001@gmail.com>
2024-01-29 00:12:53 +0530

    changes

--- src/components/home/Footer/FooterBottom.js
+++ src/components/home/Footer/FooterBottom.js
@@ -9,7 +9,7 @@ const FooterBottom = () => {
           <span className="text-md mr-[1px] mt-[2px] md:mt-0 text-center hidden md:inline-flex">
             <AiOutlineCopyright />
           </span>
-          Copyright 2024 | Print Hub LLC | All Rights Reserve
+          Copyright 2024 | Print Hub LLC | All Rights Reserved
         </p>
       </div>
     </div>
